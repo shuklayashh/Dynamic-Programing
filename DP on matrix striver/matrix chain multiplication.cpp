aa4ba6efe7e1be646fed09479ef22cefdcab92dd
Yash Shukla <shuklayash457@gmail.com>
2023-12-24 15:14:40 +0530

    memoisation

--- DP on matrix striver/matrix chain multiplication.cpp
+++ DP on matrix striver/matrix chain multiplication.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+// Recurssive code
 int f(vector<int>& arr, int i,int j){
 
     if(i ==j) return 0; // base case
@@ -38,5 +40,36 @@ int main(){
 
 
 // memoization technique
-Time comlexcity : O(N*N*N);
-Space Complexity : O(N*N)
+
+// only insert the dp array into the code we can memorise the solution in the less time comlexcity.
+int f(vector<int>& arr, int i,int j,vector<vector<int>>& dp){
+
+    if(i ==j) return 0; // base case
+
+    if(dp[i][j] != -1){
+        return dp[i][j];
+    }
+
+    int maxi = INT_MAX;
+
+    for(int k = i; k<= j-1; k++){
+
+        int ans = f(arr,i,k,dp) + f(arr,k+1,j,dp) + arr[i-1]* arr[k] * arr[j];
+
+        maxi = max(maxi,ans);
+
+    }
+
+    return maxi;
+}
+int matrixMultiplication(vector<int>& arr, int n){
+vector<vector<int>> dp(N,vector<int>(N,-1));
+    int i = 0;
+    int j = N -1;
+
+    return f(arr,i,j,dp);
+}
+
+
+Time comlexcity : O(N*N*N);// Reason - there are N*N states in which we are explixitly run a loop inside the function N times  
+Space Complexity : O(N*N) // reason - we are using auxiliary recursive  stack space 
