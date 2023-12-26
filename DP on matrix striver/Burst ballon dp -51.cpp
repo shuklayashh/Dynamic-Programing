fb0d70d4acb9f380aca551f789b7454dbadeb1a1
Yash Shukla <shuklayash457@gmail.com>
2023-12-26 21:45:12 +0530

    error handeled

--- DP on matrix striver/Burst ballon dp -51.cpp
+++ DP on matrix striver/Burst ballon dp -51.cpp
@@ -76,7 +76,7 @@ int maxCoins(vector<int>&nums){
 	
 	vector<vector<int>>dp(n+2,vector<int>(n+2,0));
 	
-	for(int i = n; i<=1; i--){
+	for(int i = n; i>=1; i--){
 		for(int j = 1; j<=n; j++){
 			
 			if(i >j) continue;
